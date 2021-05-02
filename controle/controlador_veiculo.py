dfecc4e7ed6755688d0ce9dfd3a3477d4f0c18cf
silveiraluisf <61993808+silveiraluisf@users.noreply.github.com>
2021-05-02 20:02:03 -0300

    os veículos agora vão para uma lista

--- controle/controlador_veiculo.py
+++ controle/controlador_veiculo.py
@@ -6,7 +6,7 @@ class ControladorVeiculo():
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
         self.__tela_veiculo = TelaVeiculo()
-        self.__veiculo = Veiculo
+        self.__veiculos = []
 
     def cadastrar_veiculo(self):
         dados_veiculo = self.__tela_veiculo.pega_dados_veiculo()
@@ -15,39 +15,43 @@ class ControladorVeiculo():
         veiculo = Veiculo(dados_veiculo["modelo"], dados_veiculo["ano"], dados_veiculo["placa"], 
         dados_veiculo["quilometragem"])
 
-        self.__veiculo = veiculo
+        self.__veiculos.append(veiculo)
 
-    def editar_veiculo(self):
+    #def editar_veiculo(self):
       
-        novo_modelo = input("Informe o novo modelo:")
-        self.__veiculo.modelo = novo_modelo
+    #    novo_modelo = input("Informe o novo modelo:")
+    #    self.__veiculo.modelo = novo_modelo
 
-        novo_ano = input("Informe o novo ano:")
-        self.__veiculo.ano = novo_ano
+    #   novo_ano = input("Informe o novo ano:")
+    #    self.__veiculo.ano = novo_ano
 
-        nova_placa = input("Informe a nova placa:")
-        self.__veiculo.placa = nova_placa
+    #    nova_placa = input("Informe a nova placa:")
+    #    self.__veiculo.placa = nova_placa
 
-        nova_quilometragem = input("Informe a nova quilometragem:")
-        self.__veiculo.quilometragem = nova_quilometragem
+    #    nova_quilometragem = input("Informe a nova quilometragem:")
+    #    self.__veiculo.quilometragem = nova_quilometragem
 
-        novo_veiculo = Veiculo(novo_modelo, novo_ano, nova_placa, nova_quilometragem)
-        self.__veiculo = novo_veiculo
+    #    novo_veiculo = Veiculo(novo_modelo, novo_ano, nova_placa, nova_quilometragem)
+    #    self.__veiculo = novo_veiculo
 
-    def excluir_veiculo(self):
-      veiculo = self.__veiculo 
-      del (veiculo)
+    def listar_veiculos(self):
+        for veiculo in self.__veiculos:
+            self.__tela_veiculo.mostrar_dados({"modelo": veiculo.modelo, "ano": veiculo.ano, 
+            "placa": veiculo.placa, "quilometragem": veiculo.quilometragem })
+
+    #def excluir_veiculo(self):
+    #  veiculo = self.__veiculo 
+    # del (veiculo)
         
-    def mostrar_veiculo(self):
-      veiculo = self.__veiculo
-      self.__tela_veiculo.mostrar_dados(veiculo)
+    #def mostrar_veiculo(self):
+    #  veiculo = self.__veiculo
+    #  self.__tela_veiculo.mostrar_dados(veiculo)
        
     def voltar(self):
         pass
     
     def abre_tela(self):
-        lista_opcoes = {1: self.cadastrar_veiculo, 2: self.editar_veiculo, 3: self.excluir_veiculo, 
-        4: self.mostrar_veiculo, 0: self.voltar}
+        lista_opcoes = {1: self.cadastrar_veiculo, 2: self.listar_veiculos, 0: self.voltar}
 
         lista_opcoes[self.__tela_veiculo.tela_opcoes()]()
 
