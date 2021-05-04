14b2f2c23b5c87aa812362722cd938de46e31778
silveiraluisf <61993808+silveiraluisf@users.noreply.github.com>
2021-05-04 00:15:19 -0300

    pesquisa de veiculo por placa implementada na classe veiculo

--- controle/controlador_veiculo.py
+++ controle/controlador_veiculo.py
@@ -12,10 +12,27 @@ class ControladorVeiculo():
         dados_veiculo = self.__tela_veiculo.pega_dados_veiculo()
 
         #Precisar fazer com que os veiculos criados caiam numa lista 
-        veiculo = Veiculo(dados_veiculo["modelo"], dados_veiculo["ano"], dados_veiculo["placa"], 
+        veiculo = Veiculo(dados_veiculo["modelo"], dados_veiculo["placa"], dados_veiculo["ano"], 
         dados_veiculo["quilometragem"])
 
         self.__veiculos.append(veiculo)
+
+        self.abre_tela()
+
+    def listar_veiculos(self):
+        for veiculo in self.__veiculos:
+            self.__tela_veiculo.mostrar_veiculos({"modelo": veiculo.modelo, 
+            "placa": veiculo.placa, "ano": veiculo.ano, "quilometragem": veiculo.quilometragem })
+
+        self.abre_tela()
+
+    def pesquisar_veiculo_placa(self):
+        
+        placa = self.__tela_veiculo.pesquisar_veiculo_placa()
+
+        for veiculo in self.__veiculos:
+            if placa == veiculo.placa:
+                self.__tela_veiculo.resultado_veiculo_placa({"modelo": veiculo.modelo, "placa": veiculo.placa, "ano": veiculo.ano, "quilometragem": veiculo.quilometragem})
 
     #def editar_veiculo(self):
       
@@ -34,11 +51,6 @@ class ControladorVeiculo():
     #    novo_veiculo = Veiculo(novo_modelo, novo_ano, nova_placa, nova_quilometragem)
     #    self.__veiculo = novo_veiculo
 
-    def listar_veiculos(self):
-        for veiculo in self.__veiculos:
-            self.__tela_veiculo.mostrar_veiculos({"modelo": veiculo.modelo, "ano": veiculo.ano, 
-            "placa": veiculo.placa, "quilometragem": veiculo.quilometragem })
-
     #def excluir_veiculo(self):
     #  veiculo = self.__veiculo 
     # del (veiculo)
@@ -51,7 +63,7 @@ class ControladorVeiculo():
         pass
     
     def abre_tela(self):
-        lista_opcoes = {1: self.cadastrar_veiculo, 2: self.listar_veiculos, 0: self.voltar}
+        lista_opcoes = {1: self.cadastrar_veiculo, 2: self.listar_veiculos, 3: self.pesquisar_veiculo_placa, 0: self.voltar}
 
         lista_opcoes[self.__tela_veiculo.tela_opcoes()]()
 
--- limite/tela_veiculo.py
+++ limite/tela_veiculo.py
@@ -5,6 +5,7 @@ class TelaVeiculo():
         print("Escolha sua opcao")
         print("1 - Criar veiculo")
         print("2 - Listar veiculos")
+        print("3 - Pesquisar veiculo pela placa")
         print("0 - Voltar")
         opcao = int(input("Escolha a opcao: "))
         return opcao
@@ -12,22 +13,41 @@ class TelaVeiculo():
     def pega_dados_veiculo(self):
         print("-------Incluir Veiculo-------")
         modelo = input("Modelo: ")
-        ano = input("Ano: ")
         placa = input("Placa: ")
+        ano = input("Ano: ")
         quilometragem = input("Quilometragem: ")
 
-        return {"modelo": modelo, "ano": ano, "placa": placa, "quilometragem": quilometragem}
+        return {"modelo": modelo, "placa": placa, "ano": ano, "quilometragem": quilometragem}
+
+    def mostrar_veiculos(self, dados_veiculo):
+        print("--------DADOS DO VEÍCULO---------")
+        print("MODELO: ", dados_veiculo["modelo"])
+        print("PLACA: ", dados_veiculo["ano"])
+        print("ANO: ", dados_veiculo["placa"])
+        print("QUILOMETRAGEM: ", dados_veiculo["quilometragem"], "km")
+        print("")
+
+    def pesquisar_veiculo_placa(self):
+        print("")
+        print("------ Pesquisa de veiculo pela placa:-------")
+        print("")
+        print("")
+
+        placa = input("Digite a placa a ser pesquisada: ")
+        
+        return placa
+
+    def resultado_veiculo_placa(self, dados_veiculo):
+        print("====== RESULTADO DA PESQUISA: ======")
+        print("")
+        print("MODELO DO VEICULO: ", dados_veiculo["modelo"])
+        print("PLACA: ", dados_veiculo["placa"])
+        print("ANO: ", dados_veiculo["ano"])
+        print("QUILOMETRAGEM: ", dados_veiculo["quilometragem"], "km")
+        print("")
 
     #def mostrar_dados(self, veiculo):
     #    print("Modelo do veiculo: ", veiculo.modelo)
     #    print("Ano do veiculo: ", veiculo.ano)
     #    print("Placa do veiculo: ", veiculo.placa)
     #    print("Quilometragem do veiculo: ", veiculo.quilometragem)
-
-    def mostrar_veiculos(self, dados_veiculo):
-        print("--------DADOS DO VEÍCULO---------")
-        print("MODELO: ", dados_veiculo["modelo"])
-        print("ANO: ", dados_veiculo["placa"])
-        print("PLACA: ", dados_veiculo["ano"])
-        print("QUILOMETRAGEM: ", dados_veiculo["quilometragem"], "km")
-        print("")
