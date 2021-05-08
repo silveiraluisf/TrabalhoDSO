6707242853da83f6a5777b88e5b5e297d9b0a561
silveiraluisf <61993808+silveiraluisf@users.noreply.github.com>
2021-05-08 20:11:36 -0300

    metodos de add, remove, edit e get_all da classe veiculo funcionando

--- persistencia/DAO.py
+++ persistencia/DAO.py
@@ -1,38 +1,40 @@
-#from abc import ABC, abstractmethod  
-#import pickle
+from abc import ABC, abstractmethod  
+import pickle
 
-#class DAO(ABC):
-#    def __init(self, datasource = ''):
-#        self.__datasource = datasource
-#        self.__cache = {}
-#        try:
-#            self.__load()
+class DAO(ABC):
 
-#        except FileNotFoundError:
-#            self.__dumb()
+    @abstractmethod
+    def __init__(self, datasource = ''):
+        self.__datasource = datasource
+        self.__cache = {}
+        try:
+            self.__load()
 
-#    def __dumb(self):
-#        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
+        except FileNotFoundError:
+            self.__dumb()
 
-#    def __load(self):
-#        self.__cache = pickle.load(open(self.__datasource, 'rb'))
+    def __dumb(self):
+        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
 
-#    def add(self, key, obj):
-#        self.__cache[key] = obj
-#        self.__dumb()
+    def __load(self):
+        self.__cache = pickle.load(open(self.__datasource, 'rb'))
 
-#    def get(self, key):
-#        try: 
-#            return self.__cache[key]
-#        except KeyError:
-#            pass 
+    def add(self, key, obj):
+        self.__cache[key] = obj
+        self.__dumb()
 
-#    def remove(self, key):
-#        try:
-#            self.__cache.pop(key)
-#            self.__dumb()
-#        except KeyError:
-#            pass 
+    def get(self, key):
+        try: 
+            return self.__cache[key]
+        except KeyError:
+            pass 
 
-#    def get_all(self):
-#        return self.__cache.values()
+    def remove(self, key):
+        try:
+            self.__cache.pop(key)
+            self.__dumb()
+        except KeyError:
+            pass 
+
+    def get_all(self):
+        return self.__cache.values()
--- persistencia/veiculo_dao.py
+++ persistencia/veiculo_dao.py
@@ -1,18 +1,21 @@
-#from persistencia.DAO import DAO
-#from entidade.veiculo.veiculo import Veiculo 
+from persistencia.DAO import DAO
+from entidade.veiculo.veiculo import Veiculo 
 
-#class VeiculoDAO(DAO):
-#    def __init__(self):
-#        super().__init__('veiculo.pkl')
+class VeiculoDAO(DAO):
+    def __init__(self):
+        super().__init__('veiculo.pkl')
 
-#    def add(self, veiculo: Veiculo):
-#        if (isinstance(veiculo.placa, int)) and (veiculo is not None) \
-#                and isinstance(veiculo, Veiculo):
-#            super().add(veiculo.placa, veiculo)
+    def add(self, veiculo: Veiculo):
+        if (veiculo is not None) \
+                and isinstance(veiculo, Veiculo):
+            super().add(veiculo.placa, veiculo)
 
-#    def get(self, key: int):
-#        if isinstance(key, int):
-#            return super().get(key)
+    def get(self, veiculo: Veiculo):
+        if (veiculo is not None) \
+                and isinstance(veiculo, Veiculo):
+            return super().get(veiculo.placa)
 
-#    def remove(self, key: int):
-#        return super().remove(key) 
+    def remove(self, veiculo: Veiculo):
+        if (veiculo is not None) \
+                and isinstance(veiculo, Veiculo):
+            super().remove(veiculo.placa)
