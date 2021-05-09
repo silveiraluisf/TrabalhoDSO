b15617879567d133c947a8e4303225c8842d5ba3
silveiraluisf <61993808+silveiraluisf@users.noreply.github.com>
2021-05-08 23:45:47 -0300

    sistema DAO implementado nas classe cliente pf e pj

---
+++ persistencia/cliente_dao_pj.py
@@ -0,0 +1,21 @@
+from persistencia.DAO import DAO
+from entidade.cliente.ClientePessoaJuridica import ClientePessoaJuridica
+
+class ClientePJDAO(DAO):
+    def __init__(self):
+        super().__init__('clientepj.pkl')
+    
+    def add(self, cliente: ClientePessoaJuridica):
+        if  (cliente is not None) \
+                and isinstance(cliente, ClientePessoaJuridica):
+            super().add(cliente.cnpj, cliente)
+
+    def get(self, cliente: ClientePessoaJuridica):
+        if (cliente is not None) \
+                and isinstance(cliente, ClientePessoaJuridica):
+            return super().get(cliente.cnpj)
+
+    def remove(self, cliente: ClientePessoaJuridica):
+         if  (cliente is not None) \
+                  and isinstance(cliente, ClientePessoaJuridica):
+            super().remove(cliente.cnpj)
