a159cbb557fd3849094c5653e543b488039c3cc7
NEMO DE SOUZA <53828046+nemodesouza@users.noreply.github.com>
2021-05-10 05:31:09 -0300

    OS implementado, com controlador, dao e tela

---
+++ entidade/os/os.py
@@ -0,0 +1,55 @@
+
+from entidade.cliente.ClientePessoaFisica import ClientePessoaFisica
+from entidade.cliente.ClientePessoaJuridica import ClientePessoaJuridica
+from entidade.veiculo.veiculo import Veiculo
+from entidade.revisao.revisao import Revisao
+
+class OS:
+    __numero = 1
+
+    def __init__(self, data: str, cliente: ClientePessoaFisica or ClientePessoaJuridica,
+                 veiculo: Veiculo):
+
+        self.__numero = OS.__numero
+        OS.__numero = OS.__numero + 1
+        self.__data = data
+        self.__cliente = cliente
+        self.__veiculo = veiculo
+        #self.__revisao = revisao
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data: str):
+        self.__data = data
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @cliente.setter
+    def cliente(self, cliente: ClientePessoaFisica or ClientePessoaJuridica):
+        self.__cliente = cliente
+
+    @property
+    def veiculo(self):
+        return self.__veiculo
+
+    @veiculo.setter
+    def veiculo(self, veiculo: Veiculo):
+        self.__veiculo = veiculo
+
+    @property
+    def revisao(self):
+        return self.__revisao
+
+    @revisao.setter
+    def revisao(self, revisao: Revisao):
+        self.__revisao = revisao
+
